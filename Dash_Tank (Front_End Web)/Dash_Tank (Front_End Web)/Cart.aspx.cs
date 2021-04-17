f7afcbc519e7552ce55e9d7e588efd68f41f6f29
Sean Sampson <klsampson0909@gmail.com>
2021-04-17 09:27:29 +0200

    added page for cart

---
+++ Dash_Tank (Front_End Web)/Dash_Tank (Front_End Web)/Cart.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Dash_Tank__Front_End_Web_
+{
+    public partial class Cart : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
